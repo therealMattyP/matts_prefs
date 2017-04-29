c1a544af804e09e7e609aa1ee9b22a6b27f3b493
therealMattyP <matt@mattpaquin.com>
2017-04-29 09:46:35 -0400

    removed stuff we dont need

--- Scripts/example.py
+++
@@ -1,8 +0,0 @@
-import lx, modo
-
-"""Prints a list of all selected items."""
-
-for item in modo.Scene().selected:
-    lx.out(item.name)
-
-lx.eval('layout.createOrClose EventLog "Event Log_layout" title:@macros.layouts@EventLog@ width:600 height:600 persistent:true open:true')
--- matts_prefs/commander/__init__.py
+++
@@ -1,7 +0,0 @@
-# python
-
-__version__ = "0.34"
-__author__ = "Adam"
-
-from Commander import *
-from MeshEditor import *
